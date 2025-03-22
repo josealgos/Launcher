5ebf0c607a5745c2d78eb0ec1323db57705c5697
Jose Luis Lagos <joseluislagos73@gmail.com>
2025-03-22 20:47:11 -0300

    Launcher
    Prueba

---
+++ new_version.py
@@ -0,0 +1,7 @@
+Python 3.8.0 (tags/v3.8.0:fa919fd, Oct 14 2019, 19:37:50) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> def main():
+    print("¡Esta es la nueva versión de la aplicación!")
+
+if __name__ == "__main__":
+    main()
